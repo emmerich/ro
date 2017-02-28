db20f70635d724975dfc80a6d2e0d8015d1a6d82
Steven Hall <shne24@gmail.com>
2017-02-28 09:05:46 +0100

    wip

--- src/background/index.js
+++ src/background/index.js
@@ -1,21 +1,44 @@
 import fetch from 'node-fetch'
+import FormData from 'form-data'
 import { writeFileSync, ensureDirSync, emptyDirSync } from 'fs-extra'
-import { uniq, flatten, uniqWith, assocPath, omit } from 'ramda'
+import { uniq, flatten, uniqWith, assocPath, omit, pick } from 'ramda'
 import { join } from 'path'
 
-const id = "6mP7uxSzgb3MFuyc8I8OlT8vcDk"
+let id = "6mP7uxSzgb3MFuyc8I8OlT8vcDk"
+
+const refreshID = async () => {
+  console.log('Refreshing reddit ID')
+  // const body = new FormData()
+  // body.append('grant_type', 'client_credentials')
+
+  // console.log('Body', body)
+
+  const res = await fetch('https://www.reddit.com/api/v1/access_token', {
+    method: 'post',
+    headers: {
+      'User-Agent': 'JohnnyBravo/0.1 by JBTopGuy',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic N1B0RU93Rkk5OUJ2OFE6RE1RR2x1WG16SnFINjZzNU1fU3cwMS1mLUQ4'
+    },
+    body: 'grant_type=client_credentials'
+  })
+
+  console.log('res', res.status)
+  const json = await res.json()
+  console.log('res', json)
+  id = json.access_token
+//   {
+//   "access_token": "n9prJuEEYfSosXxqLlhn_ljic5U",
+//   "token_type": "bearer",
+//   "expires_in": 3600,
+//   "scope": "*"
+// }
+}
 
 const get = async (url) => {
   let actualURL = `https://oauth.reddit.com${url}`
 
-  // if(after) {
-  //   actualURL = `${actualURL}&after=${after}`
-  // }
-  //
-  // if(count) {
-  //   actualURL = `${actualURL}&count=${count}`
-  // }
-
+  console.log('get', id)
   const res = await fetch(actualURL, {
     headers: {
       'Authorization': `bearer ${id}`,
@@ -24,12 +47,19 @@ const get = async (url) => {
   })
 
   console.log(actualURL, res.status)
+
+  if(res.status === 401) {
+    // ID has expired, need a new one.
+    await refreshID()
+    return get(url)
+  }
+
   return res.json()
 }
 
 const getPosts = async (url) => {
   const resp = await get(url)
-  return resp.data.children
+  return resp.data.children.map((post) => post.data)
 }
 
 const getComments = async (url) => {
@@ -58,193 +88,6 @@ const getComments = async (url) => {
   return resp.reduce((res, rootArray) => res.concat(flattenCommentArray(rootArray)), [])
 }
 
-let c = 0
-const api = async (url) => {
-  c++
-  let result = []
-
-
-
-  const loop = async (after, count) => {
-    const res = await get(after, count)
-    result = result.concat(res.children)
-
-    if(res.after && result.length < 3) {
-      return loop(res.after, result.length)
-    } else {
-      return result
-    }
-  }
-
-  return loop(null, null)
-}
-
-// api(`/r/soccer/about/contributors`).then((res) => console.log(res))
-//
-// const search = (term) => {
-//   fetch(`https://oauth.reddit.com/api/subreddits_by_topic?query=${term}`, {
-//     headers: {
-//       'Authorization': `bearer ${id}`,
-//       'User-Agent': "JohnnyBravo/0.1 by JBTopGuy"
-//     }
-//   })
-//     .then((res) => {
-//       console.log(res)
-//       return res.json()
-//     })
-//     .then((resjson) => console.log(resjson))
-// }
-
-// Backend
-// Authors of top submissions in the past month
-  // Treat self posts more importantly than links.
-  // Get the top comments too
-
-// Authors of submissions in the past week
-  // Treat self posts more importantly than links.
-  // Get the top comments too
-
-
-// request per subreddit (worst case) = 500 + 5 = 505
-// requests per day = 86400 = 170 subreddits per day
-// requests per week = 1197 subreddits per week
-
-// probably like 5000 subreddits in total with a decent amout of subscribers
-// 5000 subreddits run once a week = 15000 requests per week (3 request per subreddit)
-
-// Run subreddits on demand, only the most important first. The others can be
-// on the backburner. Not all 5000 are that important.
-
-// Maximum requests
-  // per minute 60
-  // per hour 3600
-  // per day 86400
-  // per week 604800
-
-
-// Front end
-
-const posts = [
-  {
-    name: 'about',
-    type: 'info',
-    path: ['about'],
-    api: '/about'
-  },
-
-  // {
-  //   name: 'rising',
-  //   path: ['posts', 'rising'],
-  //   api: '/rising'
-  // },
-
-  {
-    name: 'weekly',
-    path: ['posts', 'weekly'],
-    api: '/top?t=week&limit=100'
-  },
-
-  {
-    name: 'monthly',
-    path: ['posts', 'monthly'],
-    api: '/top?t=month&limit=100'
-  },
-
-  {
-    name: 'hot',
-    path: ['posts', 'hot'],
-    api: '/hot?limit=100'
-  },
-  //
-  // {
-  //   name: 'topDaily',
-  //   path: ['posts', 'top100_daily'],
-  //   api: '/top?t=day&limit=100'
-  // }
-]
-
-const userInfo = [
-  {
-    name: 'about',
-    path: ['about'],
-    api: '/about'
-  },
-
-  {
-    name: 'top_100_submitted_in_past_month',
-    path: ['posts', 'top100pastMonth'],
-    api: '/submitted?limit=100&sort=top&t=month'
-  },
-
-  {
-    name: 'top_100_comments_in_past_month',
-    path: ['comments', 'top100pastMonth'],
-    api: '/comments?limit=100&sort=top&t=month'
-  },
-
-  // {
-  //   name: '20_recently_submitted',
-  //   path: ['posts', 'recent20'],
-  //   api: '/submitted?limit=20&sort=new'
-  // },
-  //
-  // {
-  //   name: '20_recently_commented',
-  //   path: ['comments', 'recent20'],
-  //   api: '/comments?limit=20&sort=top'
-  // },
-
-
-  // {
-  //   name: 'upvoted',
-  //   api: '/upvoted?limit=100'
-  // },
-  //
-  // {
-  //   name: 'downvoted',
-  //   api: '/downvoted?limit=100'
-  // }
-]
-
-// const get = async (baseURL, measurements, root) => {
-//   // const promises = Promise.all(
-//   //   measurements.map((measurement) => {
-//   //     return api(baseURL + measurement.api)
-//   //       .then((res) => Object.assign({}, res, { measurement: measurement.name }))
-//   //   })
-//   // )
-//
-//   return measurements.reduce(async (outputPromise, measurement) => {
-//     const output = await outputPromise
-//     const result = await api(baseURL + measurement.api)
-//     return assocPath(measurement.path, result, output)
-//   }, Promise.resolve(root))
-//
-//   // return promises.then((results) => {
-//   //   return results.reduce((output, result) => {
-//   //     const data = result.data.children.map((r) => Object.assign({}, r, {
-//   //       measurement: result.measurement
-//   //     }))
-//   //
-//   //     return output.concat(data)
-//   //   }, [])
-//   // })
-// }
-
-// const user = async (username) => {
-//   const baseURL = `/user/${username}`
-//   const result = await get(baseURL, userInfo, { name: username })
-//
-//   return result
-// }
-//
-// const subreddit = async (name) => {
-//   const baseURL = `/r/${name}`
-//   const result = await get(baseURL, posts, { name })
-//   // const uniquePosts = uniqWith((lhs, rhs) => lhs.data.id === rhs.data.id, results)
-//
-//   return result
-// }
 
 const DATA_DIR = join(__dirname, '..', '..', 'data')
 
@@ -252,12 +95,13 @@ const getSubreddit = async (subreddit) => {
   const posts = await getPosts(`/r/${subreddit}/hot?limit=25`)
 
   const postsWithComments = await Promise.all(posts.map(async (post) => {
-    const post_comments = await getComments(`/r/${subreddit}/comments/${post.data.id}?depth=5&sort=top`)
-    return Object.assign({}, post.data, { comments: post_comments })
+    const post_comments = await getComments(`/r/${subreddit}/comments/${post.id}?depth=5&sort=top`)
+    return Object.assign({}, post, { comments: post_comments })
   }))
 
   return { subreddit, posts: postsWithComments }
 }
+
 
 const getAll = async (subreddits) => {
   const subredditDataDir = join(DATA_DIR)
@@ -311,6 +155,9 @@ const getAll = async (subreddits) => {
 }
 
 
+
+
+
 // For a subreddit /r/
   // Current Front Page Posts
   // Top 100 posts in the past week / month
@@ -331,4 +178,6 @@ const getAll = async (subreddits) => {
   // Wiki Contributors - Who contributes long-term content?
   // X-Posts, other subreddits mentioned in Sidebar, Wiki - Which subreddits share content with this one?
 
-getAll(['soccer'])
+const subreddits = ['soccer']
+getAll(subreddits)
+setInterval(() => getAll(subreddits), 1000 * 60 * 60)
